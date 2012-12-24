b8be466ca605a752c66cd35b061c0c7e74dbcca3
Zhao Tang <zxt@zhaotang.ca>
2012-12-24 18:03:50 -0500

    add view functions

--- quotr.py
+++ quotr.py
@@ -39,5 +39,41 @@ def teardown_request(exception):
     if hasattr(g, 'db'):
         g.db.close()
 
+# views
+@app.route('/')
+def show_quotes():
+    quotes = query_db('SELECT quote, author FROM quotes ORDER BY id DESC')
+    return render_template('show_quotes.html', quotes=quotes)
+
+@app.route('/add', methods=['POST'])
+def add_quote():
+    if not session.get('logged_in'):
+        abort(401)
+    g.db.execute('INSERT INTO quotes (quote, author) VALUES (?, ?)',
+                 [request.form['quote'], request.form['author']])
+    g.db.commit()
+    flash('New quote successfully added.')
+    return redirect(url_for('show_quotes'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('Login successful.')
+            return redirect(url_for('show_quotes'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('Logout successful.')
+    return redirect(url_for('show_quotes'))
+
 if __name__ == '__main__':
     app.run()
