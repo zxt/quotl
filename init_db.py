01ce7e8e553a9bf8c25f477eaa4d7b79737af592
Zhao Tang <zxt@zhaotang.ca>
2012-12-24 18:01:10 -0500

    add db functions

---
+++ init_db.py
@@ -0,0 +1,2 @@
+from quotr import init_db
+init_db()
--- quotr.py
+++ quotr.py
@@ -14,5 +14,30 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_envvar('QUOTR_SETTINGS', silent=True)
 
+# database
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('db/schema.sql') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+def query_db(query, args=(), one=False):
+    cur = g.db.execute(query, args)
+    rv = [dict((cur.description[idx][0], value)
+            for idx, value in enumerate(row)) for row in cur.fetchall()]
+    return (rv[0] if rv else None) if one else rv
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+    if hasattr(g, 'db'):
+        g.db.close()
+
 if __name__ == '__main__':
     app.run()
