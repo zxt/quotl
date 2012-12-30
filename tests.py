b9124fb00145660c9cc22e88054002a25bea1c4b
Zhao Tang <zxt@zhaotang.ca>
2012-12-29 23:43:56 -0500

    add unit tests

--- tests.py
+++ tests.py
@@ -15,5 +15,38 @@ class QuotrTestCase(unittest.TestCase):
         os.close(self.db_fd)
         os.unlink(quotr.app.config['DATABASE'])
 
+    def login(self, username, password):
+        return self.app.post('/login', data=dict(
+            username=username,
+            password=password
+        ), follow_redirects=True)
+
+    def logout(self):
+        return self.app.get('/logout', follow_redirects=True)
+
+    def test_empty_db(self):
+        rv = self.app.get('/')
+        assert 'No quotes found. How about adding some?' in rv.data
+
+    def test_login_logout(self):
+        rv = self.login(quotr.app.config['USERNAME'], quotr.app.config['PASSWORD'])
+        assert 'Login successful' in rv.data
+        rv = self.logout()
+        assert 'Logout successful' in rv.data
+        rv = self.login('invalidusername', quotr.app.config['PASSWORD'])
+        assert 'Invalid username' in rv.data
+        rv = self.login(quotr.app.config['USERNAME'], 'invalidpassword')
+        assert 'Invalid password' in rv.data
+
+    def test_add_quote(self):
+        self.login('admin', 'default')
+        rv = self.app.post('/add', data=dict(
+            quote='This is a very <strong>awesome</strong> quote.',
+            author='<Anonymous>'
+        ), follow_redirects=True)
+        assert 'No quotes found. How about adding some?' not in rv.data
+        assert 'This is a very <strong>awesome</strong> quote.' in rv.data
+        assert '&lt;Anonymous&gt;' in rv.data
+
 if __name__ == '__main__':
     unittest.main()
