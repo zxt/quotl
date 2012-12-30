4bcca726af311e46fe6b56fff181e38ae038a1a3
Zhao Tang <zxt@zhaotang.ca>
2012-12-29 23:06:32 -0500

    change to use text instead of string in schema, correct type name recognized by sqlite

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,6 @@
 drop table if exists quotes;
 create table quotes (
   id integer primary key autoincrement,
-  quote string not null,
-  author string not null
+  quote text not null,
+  author text not null
 );
