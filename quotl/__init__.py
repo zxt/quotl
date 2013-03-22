c7b334c294816eb96c3a8845a573247d5ab09d4c
Zhao Tang <zxt@zhaotang.ca>
2013-03-22 00:58:38 -0400

    move configurations into a separate file

--- quotl/__init__.py
+++ quotl/__init__.py
@@ -1,19 +1,13 @@
 from flask import Flask, g
 from contextlib import closing
 
-# configuration
-DATABASE = 'quotl/db/quotl.db'
-DEBUG = True
-SECRET_KEY = 'development key'
-USERNAME = 'admin'
-PASSWORD = 'default'
-
 app = Flask(__name__)
-app.config.from_object(__name__)
-app.config.from_envvar('QUOTL_SETTINGS', silent=True)
 
 import quotl.views
 import quotl.db as db
+
+app.config.from_object('quotl.config')
+app.config.from_envvar('QUOTL_SETTINGS', silent=True)
 
 @app.before_request
 def before_request():
---
+++ quotl/config.py
@@ -0,0 +1,5 @@
+DATABASE = 'quotl/db/quotl.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
